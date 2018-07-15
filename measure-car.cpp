9be1992aede787e70bb19d26579df4b96dd30761
Jan Horacek <me@apophis.cz>
2018-07-15 12:50:03 +0200

    Add serial port code from example.

--- measure-car.cpp
+++ measure-car.cpp
@@ -1,1 +1,19 @@
+#include <QCoreApplication>
+
 #include "measure-car.h"
+
+MeasureCar::MeasureCar(QSerialPort *serialPort, QObject *parent) :
+	QObject(parent),
+	m_serialPort(serialPort) {
+	connect(m_serialPort, &QSerialPort::readyRead, this, &MeasureCar::handleReadyRead);
+	connect(m_serialPort, &QSerialPort::errorOccurred, this, &MeasureCar::handleError);
+}
+
+void MeasureCar::handleReadyRead() {
+	m_readData.append(m_serialPort->readAll());
+}
+
+void MeasureCar::handleError(QSerialPort::SerialPortError serialPortError) {
+	// TODO
+	(void)serialPortError;
+}
--- measure-car.h
+++ measure-car.h
@@ -1,10 +1,23 @@
 #ifndef _MEASURE_CAR_H_
 #define _MEASURE_CAR_H_
 
+#include <QByteArray>
 #include <QSerialPort>
+#include <QTimer>
 
-class MeasureCar {
+class MeasureCar : public QObject {
+	Q_OBJECT
+
 public:
+	explicit MeasureCar(QSerialPort *serialPort, QObject *parent = nullptr);
+
+private slots:
+	void handleReadyRead();
+	void handleError(QSerialPort::SerialPortError error);
+
+private:
+	QSerialPort *m_serialPort = nullptr;
+	QByteArray m_readData;
 };
 
 #endif
